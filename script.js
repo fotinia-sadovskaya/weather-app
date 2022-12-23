65ea0f98c4d3b7a13f9ecc10228f9d2f706ca628
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2022-12-23 17:09:42 +0200

    Changed JS file

--- script.js
+++ script.js
@@ -1,1 +1,129 @@
-alert("")
+//Current Date and Time
+function showDate() {
+  let now = new Date();
+
+  let date = now.getDate();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+
+  let months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "Jule",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${day}, ${month} ${date}`;
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let time = document.querySelector("h3");
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  time.innerHTML = `${hours}:${minutes}`;
+}
+showDate();
+
+//City Search
+
+/*function showCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let cityName = document.querySelector("h1");
+  cityName.innerHTML = cityInput.value;
+}
+let citySearch = document.querySelector("#city-search");
+citySearch.addEventListener("submit", showCity);*/
+
+function showWeather(response) {
+  let cityName = document.querySelector("h1");
+  cityName.innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "583543d42b8605a7c28a20d072be705c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+// Bonus Celsium and Fahrenheit
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let temperatureElementFahrenheit = Math.round(
+    (9 / 5) * temperatureElement.innerHTML + 32
+  );
+  temperatureElement.innerHTML = temperatureElementFahrenheit;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `1`;
+}
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+//Currant Location Search
+function searchLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "583543d42b8605a7c28a20d072be705c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#currant-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
